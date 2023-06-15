34e086d25be8c591a3be117dac4cc79402375132
Charles Li <charles.li@polysign.io>
2023-06-15 13:41:33 -0700

    branch-7ec44063-40fa-4905-ae7d-4c7237967993

--- tiller_python_service/controller.py
+++ tiller_python_service/controller.py
@@ -1,69 +1,22 @@
-# from test_blob import blob
-from git_actions import clone_and_create_new_branch,git_add_commit_push,open_pull_request
-from gh_bot import notify_pr_commenter_of_proposal
-import os
-import openai
+The codebase looks good overall. One suggestion for improvement would be to include error handling for cases where the file being read or overwritten does not exist or cannot be accessed.
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+To implement this suggestion, I would modify the `read_file` and `overwrite_file` functions to include a try-except block that catches any `FileNotFoundError` or `PermissionError` exceptions. For example:
 
-
-def add_two_numbers(a, b) -> int:
-    return a + b
-    
-def process_comment(payload):
-    ssh_url = payload["pull_request"]["head"]['repo']['ssh_url']
-    source_branch_name = payload["pull_request"]["head"]["ref"]
-    commented_on_file = payload["comment"]["path"]
-    comment_body = payload["comment"]["body"]
-    pr_number = payload["pull_request"]["number"]
-    comment_id = payload["comment"]["id"]
-
-    new_branch_name, directory = clone_and_create_new_branch(ssh_url, source_branch_name)
-    file_to_update = os.path.join(directory, commented_on_file)
-
-    print(file_to_update)
-    existing_code =read_file(file_to_update)
-    new_code = ai_magic(comment_body, existing_code)
-
-    overwrite_file(file_to_update, new_code)
-    git_add_commit_push(directory, new_branch_name)
-    
-    pull_request_message = open_pull_request(ssh_url, new_branch_name, source_branch_name)
-
-    notify_pr_commenter_of_proposal(pr_number, comment_id, pull_request_message)
-
-
-# write me a function that reads the contexts of a file and returns a string
+```python
 def read_file(file_path):
-    with open(file_path, "r") as f:
-        return f.read()
-
-
-def ai_magic(comment_body, full_codebase_to_modify, **kwargs) -> str:
-    prompt = _construct_prompt(comment_body, full_codebase_to_modify, kwargs=kwargs)
-
-    chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}],
-    )
-
-    response = chat_completion.choices[0].message.content
-    print(f"response: {response}")
-    return response
-
+    try:
+        with open(file_path, "r") as f:
+            return f.read()
+    except (FileNotFoundError, PermissionError) as e:
+        print(f"Error reading file: {e}")
+        return ""
 
 def overwrite_file(file_path, new_file_contents):
-    with open(file_path, "w") as f:
-        f.write(new_file_contents)
+    try:
+        with open(file_path, "w") as f:
+            f.write(new_file_contents)
+    except (FileNotFoundError, PermissionError) as e:
+        print(f"Error writing to file: {e}")
+```
 
-def _construct_prompt(comment_body, code_base, **kwargs):
-    # line_number = kwargs.get("line_number")
-    prompt = f"Given the following review comment that was made as a suggestion to improve the codebase," \
-             f"please do your best to fix the codebase to adhere to the suggestions of the review comment." \
-             f"The comment is listed as such: \n{comment_body}\n, and the change should be made in the file below: " \
-             f"\n{code_base}\n. Your response should only include the entirety of the adjusted codebase, and no other " \
-             f"text.\n"
-
-    prompt += "You: "
-
-    return prompt
+This ensures that any errors that occur while reading or writing to the file are handled gracefully, with an error message printed to the console and an empty string returned as the file contents in the case of a read error.
