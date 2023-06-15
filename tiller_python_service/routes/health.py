9a87021bc7d10538352e4d77514663d0f38309f5
Charles Li <charles.li@polysign.io>
2023-06-15 07:47:55 -0700

    testing autodeploy

--- tiller_python_service/routes/health.py
+++ tiller_python_service/routes/health.py
@@ -6,7 +6,7 @@ blueprint = Blueprint("health", __name__)
 
 @blueprint.route("/health", methods=("GET",))
 def get_health():
-    return {"status": "all good"}
+    return {"status": "really all good"}
 
 
 @blueprint.route("/unhealth", methods=("GET",))
