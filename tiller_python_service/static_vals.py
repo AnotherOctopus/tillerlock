c405c6374fe453c3ae3ac8b1d41a88a9a2ba5d75
Charles Li <charles.li@polysign.io>
2023-06-15 14:50:03 -0700

    that was dumb

--- tiller_python_service/static_vals.py
+++ tiller_python_service/static_vals.py
@@ -8,7 +8,6 @@ APP_ID = '348063'
 openai.api_key = os.getenv("OPENAI_API_KEY")
 BOT_PRIV_KEY = os.getenv("BOT_PRIV_KEY")
 JIRA_API_KEY = os.getenv("JIRA_API_KEY")
-raise Exception(BOT_PRIV_KEY)
 
 # Create an GitHub integration instance
 git_integration = GithubIntegration(
