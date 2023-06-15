f8abc4591fd455af966ba6f2f5ddf69f3be8d924
Charles Li <charles.li@polysign.io>
2023-06-15 13:33:48 -0700

    branch-0078ecb0-7812-4463-a603-38e7029a4739

--- tiller_python_service/controller.py
+++ tiller_python_service/controller.py
@@ -9,7 +9,14 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def add_two_numbers(a, b) -> int:
     return a + b
-    
+
+
+# write me a function that reads the contents of a file and returns a string
+def read_file(file_path):
+    with open(file_path, "r") as f:
+        return f.read()
+
+
 def process_comment(payload):
     ssh_url = payload["pull_request"]["head"]['repo']['ssh_url']
     source_branch_name = payload["pull_request"]["head"]["ref"]
@@ -22,7 +29,7 @@ def process_comment(payload):
     file_to_update = os.path.join(directory, commented_on_file)
 
     print(file_to_update)
-    existing_code =read_file(file_to_update)
+    existing_code = read_file(file_to_update)
     new_code = ai_magic(comment_body, existing_code)
 
     overwrite_file(file_to_update, new_code)
@@ -33,21 +40,19 @@ def process_comment(payload):
     notify_pr_commenter_of_proposal(pr_number, comment_id, pull_request_message)
 
 
-# write me a function that reads the contexts of a file and returns a string
-def read_file(file_path):
-    with open(file_path, "r") as f:
-        return f.read()
-
-
 def ai_magic(comment_body, full_codebase_to_modify, **kwargs) -> str:
     prompt = _construct_prompt(comment_body, full_codebase_to_modify, kwargs=kwargs)
 
-    chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}],
+    chat_completion = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
     )
 
-    response = chat_completion.choices[0].message.content
+    response = chat_completion.choices[0].text
     print(f"response: {response}")
     return response
 
@@ -56,14 +61,12 @@ def overwrite_file(file_path, new_file_contents):
     with open(file_path, "w") as f:
         f.write(new_file_contents)
 
+
 def _construct_prompt(comment_body, code_base, **kwargs):
-    # line_number = kwargs.get("line_number")
-    prompt = f"Given the following review comment that was made as a suggestion to improve the codebase," \
+    prompt = f"Given the following review comment that was made as a suggestion to improve the codebase, " \
              f"please do your best to fix the codebase to adhere to the suggestions of the review comment." \
              f"The comment is listed as such: \n{comment_body}\n, and the change should be made in the file below: " \
              f"\n{code_base}\n. Your response should only include the entirety of the adjusted codebase, and no other " \
-             f"text.\n"
-
-    prompt += "You: "
+             f"text.\nYou: "
 
     return prompt
