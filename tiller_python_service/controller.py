b630c745bb37ef13c216b5770052764fd52be67d
Charles Li <charles.li@polysign.io>
2023-06-15 13:36:30 -0700

    branch-d6c60e55-9a1b-4f86-9ac7-857ec9d1b72b

--- tiller_python_service/controller.py
+++ tiller_python_service/controller.py
@@ -1,5 +1,5 @@
 # from test_blob import blob
-from git_actions import clone_and_create_new_branch,git_add_commit_push,open_pull_request
+from git_actions import clone_and_create_new_branch, git_add_commit_push, open_pull_request
 from gh_bot import notify_pr_commenter_of_proposal
 import os
 import openai
@@ -9,9 +9,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def add_two_numbers(a, b) -> int:
     return a + b
-    
+
+
 def process_comment(payload):
-    ssh_url = payload["pull_request"]["head"]['repo']['ssh_url']
+    ssh_url = payload["pull_request"]["head"]["repo"]["ssh_url"]
     source_branch_name = payload["pull_request"]["head"]["ref"]
     commented_on_file = payload["comment"]["path"]
     comment_body = payload["comment"]["body"]
@@ -22,18 +23,17 @@ def process_comment(payload):
     file_to_update = os.path.join(directory, commented_on_file)
 
     print(file_to_update)
-    existing_code =read_file(file_to_update)
+    existing_code = read_file(file_to_update)
     new_code = ai_magic(comment_body, existing_code)
 
     overwrite_file(file_to_update, new_code)
     git_add_commit_push(directory, new_branch_name)
-    
+
     pull_request_message = open_pull_request(ssh_url, new_branch_name, source_branch_name)
 
     notify_pr_commenter_of_proposal(pr_number, comment_id, pull_request_message)
 
 
-# write me a function that reads the contexts of a file and returns a string
 def read_file(file_path):
     with open(file_path, "r") as f:
         return f.read()
@@ -42,12 +42,20 @@ def read_file(file_path):
 def ai_magic(comment_body, full_codebase_to_modify, **kwargs) -> str:
     prompt = _construct_prompt(comment_body, full_codebase_to_modify, kwargs=kwargs)
 
-    chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}],
+    prompt += full_codebase_to_modify
+
+    chat_completion = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=2048,
+        n=1,
+        stop=None,
+        temperature=0.5,
+        frequency_penalty=0,
+        presence_penalty=0,
     )
 
-    response = chat_completion.choices[0].message.content
+    response = chat_completion.choices[0].text
     print(f"response: {response}")
     return response
 
@@ -56,13 +64,13 @@ def overwrite_file(file_path, new_file_contents):
     with open(file_path, "w") as f:
         f.write(new_file_contents)
 
+
 def _construct_prompt(comment_body, code_base, **kwargs):
-    # line_number = kwargs.get("line_number")
-    prompt = f"Given the following review comment that was made as a suggestion to improve the codebase," \
+    prompt = f"Given the following review comment that was made as a suggestion to improve the codebase, " \
              f"please do your best to fix the codebase to adhere to the suggestions of the review comment." \
-             f"The comment is listed as such: \n{comment_body}\n, and the change should be made in the file below: " \
-             f"\n{code_base}\n. Your response should only include the entirety of the adjusted codebase, and no other " \
-             f"text.\n"
+             f" The comment is listed as such: \n{comment_body}\n, and the change should be made in the file below:" \
+             f" \n{code_base}\n. Your response should only include the entirety of the adjusted codebase, " \
+             f"and no other text.\n"
 
     prompt += "You: "
 
