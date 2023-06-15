f87296ba01a9a46db2e3a80161645284ccded769
beasknees <matt.beasley@polysign.io>
2023-06-15 10:00:21 -0500

    add higher-level function

--- tiller_python_service/git_actions.py
+++ tiller_python_service/git_actions.py
@@ -1,6 +1,7 @@
 from git import Repo, GitCommandError
 import subprocess
 import tempfile
+import uuid
 import os
 
 def clone_repo(url):
@@ -70,5 +71,44 @@ def git_checkout_branch(repo_path, branch_name):
     except GitCommandError as e:
         return str(e)
 
-dir = clone_repo("git@github.com:AnotherOctopus/tillerlock.git")
-print(dir)
+def generate_random_branch_name():
+    """
+    Generate a random branch name.
+
+    Returns:
+        str: The generated branch name.
+    """
+    return "branch-" + str(uuid.uuid4())
+
+def clone_and_create_new_branch(repo_url, initial_branch):
+    """
+    Clone a repository, switch to a specified branch, and create a new branch with a random name.
+
+    Examples:
+        branch = clone_and_create_new_branch("git@github.com:AnotherOctopus/tillerlock.git", "git-functions")
+
+    Parameters:
+        repo_url (str): The URL of the git repository.
+        initial_branch (str): The name of the branch to switch to after cloning.
+
+    Returns:
+        str: A message about the operation's success or the error message.
+    """
+    # Clone the repo
+    repo_path = clone_repo(repo_url)
+    if repo_path is None:
+        return "Failed to clone repository"
+
+    # Switch to the initial branch
+    checkout_message = git_checkout_branch(repo_path, initial_branch)
+    if not checkout_message.startswith("Switched"):
+        return "Failed to switch to initial branch: " + checkout_message
+
+    # Create a new branch with a random name and switch to it
+    new_branch_name = generate_random_branch_name()
+    try:
+        repo = Repo(repo_path)
+        repo.git.checkout('-b', new_branch_name)
+        return "Switched to new branch: " + new_branch_name
+    except GitCommandError as e:
+        return "Failed to switch to new branch: " + str(e)
