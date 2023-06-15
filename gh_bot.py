12821b26952b4a6aaafe0b44efafa4264ad16d0f
Charles Li <charles.li@polysign.io>
2023-06-15 07:37:09 -0700

    save

--- gh_bot.py
+++ gh_bot.py
@@ -1,18 +1,18 @@
 
 from github import Github, GithubIntegration
-from static_vals import git_integration
+from static_vals import git_integration, REPO_NAME, OWNER
 
 def respond_to_pr_comment(pr_number: int, comment_id: str, body: str):
-    owner = payload['repository']['owner']['login']
-    repo_name = payload['repository']['name']
+    owner = OWNER 
+    repo_name = REPO_NAME 
 
     # Get a git connection as our bot
     # Here is where we are getting the permission to talk as our bot and not
     # as a Python webservice
     git_connection = Github(
         login_or_token=git_integration.get_access_token(
-            git_integration.get_installation(owner, repo_name).id
+            git_integration.get_repo_installation(owner, repo_name).id
         ).token
     )
     repo = git_connection.get_repo(f"{owner}/{repo_name}")
-    repo.get_pull(pr_number).create_review_comment_reply()
+    print(repo.get_pull(pr_number).comments)
