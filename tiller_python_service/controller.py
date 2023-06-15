4cbc0d9262bf590077459bbd08508fa37eb7ca25
Charles Li <charles.li@polysign.io>
2023-06-15 13:39:06 -0700

    branch-afb5dc8d-4a75-410b-b4a0-bebb10809e21

--- tiller_python_service/controller.py
+++ tiller_python_service/controller.py
@@ -20,9 +20,7 @@ def process_comment(payload):
 
     new_branch_name, directory = clone_and_create_new_branch(ssh_url, source_branch_name)
     file_to_update = os.path.join(directory, commented_on_file)
-
-    print(file_to_update)
-    existing_code =read_file(file_to_update)
+    existing_code = read_file(file_to_update)
     new_code = ai_magic(comment_body, existing_code)
 
     overwrite_file(file_to_update, new_code)
@@ -33,37 +31,39 @@ def process_comment(payload):
     notify_pr_commenter_of_proposal(pr_number, comment_id, pull_request_message)
 
 
-# write me a function that reads the contexts of a file and returns a string
+# write me a function that reads the contents of a file and returns a string
 def read_file(file_path):
     with open(file_path, "r") as f:
         return f.read()
 
 
-def ai_magic(comment_body, full_codebase_to_modify, **kwargs) -> str:
-    prompt = _construct_prompt(comment_body, full_codebase_to_modify, kwargs=kwargs)
+def ai_magic(comment_body, code_base, **kwargs) -> str:
+    prompt = f"The following review comment was made as a suggestion to improve the codebase:" \
+             f"\n{comment_body}\n" \
+             f"The change should be made in the following file:\n{code_base}\n" \
+             f"Please implement the suggested changes in the codebase shown below:\n"
 
-    chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}],
+    prompt += code_base
+
+    chat_completion = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        timeout=60,
     )
 
-    response = chat_completion.choices[0].message.content
-    print(f"response: {response}")
-    return response
+    suggestions = chat_completion.choices[0].text
+
+    # get rid of the prompt in the output
+    adjusted_code = suggestions.replace(prompt, "")
+
+    print(f"response: {adjusted_code}")
+    return adjusted_code
 
 
 def overwrite_file(file_path, new_file_contents):
     with open(file_path, "w") as f:
         f.write(new_file_contents)
-
-def _construct_prompt(comment_body, code_base, **kwargs):
-    # line_number = kwargs.get("line_number")
-    prompt = f"Given the following review comment that was made as a suggestion to improve the codebase," \
-             f"please do your best to fix the codebase to adhere to the suggestions of the review comment." \
-             f"The comment is listed as such: \n{comment_body}\n, and the change should be made in the file below: " \
-             f"\n{code_base}\n. Your response should only include the entirety of the adjusted codebase, and no other " \
-             f"text.\n"
-
-    prompt += "You: "
-
-    return prompt
