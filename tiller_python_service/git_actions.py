034f94959ff8fc33d878793931cbbbe309872723
Charles Li <charles.li@polysign.io>
2023-06-16 11:26:40 -0700

    adding sorting function

--- tiller_python_service/git_actions.py
+++ tiller_python_service/git_actions.py
@@ -10,6 +10,31 @@ import os
 import re
 import openai
 
+
+def my_new_sorting_function(arr):
+    n = len(arr)
+    # optimize code, so if the array is already sorted, it doesn't need
+    # to go through the entire process
+    swapped = False
+    # Traverse through all array elements
+    for i in range(n-1):
+        # range(n) also work but outer loop will
+        # repeat one time more than needed.
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+         
+        if not swapped:
+            # if we haven't needed to make a single swap, we
+            # can just exit the main loop.
+            return
+
 def clone_repo(url):
     """
     Clone a git repository from a specified URL into a randomly named directory within /tmp.
