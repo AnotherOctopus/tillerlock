5a4edc26c606b95d967603a330951df9ce628098
root <root@4eac8e814a48>
2023-06-16 03:42:54 +0000

    branch-12c9d844-0976-44bb-a3a8-f1e895c9c47b

--- tiller_python_service/controller.py
+++ tiller_python_service/controller.py
@@ -36,6 +36,38 @@ def my_new_sort(arr):
             # can just exit the main loop.
             return
 
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2 # Finding the mid of the array
+        L = arr[:mid] # Dividing the array elements 
+        R = arr[mid:] # into 2 halves
+ 
+        merge_sort(L) # Sorting the first half
+        merge_sort(R) # Sorting the second half
+ 
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
 def is_valid_python(code):
     try:
         ast.parse(code)
@@ -109,7 +141,7 @@ def ai_magic(comment_body, full_codebase_to_modify, **kwargs) -> str:
         )
         print(chat_completion)
         for response in chat_completion.choices:
-            msg = response.message.content.replace("```", "")
+            msg = response.message.content.replace("", "")
             if is_valid_python(msg):
                 response = msg
                 print(f"response: {msg}")
