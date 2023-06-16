401cebd39aaf3cc37bed0a071c6a017eade34ab6
root <root@c295cde73def>
2023-06-16 18:51:19 +0000

    branch-1aebdbf8-72eb-4b7c-b488-891b1be266da

--- tiller_python_service/error_handlers.py
+++ tiller_python_service/error_handlers.py
@@ -1,33 +1,45 @@
 import logging
-from typing import Tuple
+from typing import List, Tuple
 
 from flask import Response, jsonify
 
 LOGGER = logging.getLogger(__name__)
 
-def my_new_sorting_function(arr):
-    n = len(arr)
-    # optimize code, so if the array is already sorted, it doesn't need
-    # to go through the entire process
-    swapped = False
-    # Traverse through all array elements
-    for i in range(n-1):
-        # range(n) also work but outer loop will
-        # repeat one time more than needed.
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
- 
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j + 1]:
-                swapped = True
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-         
-        if not swapped:
-            # if we haven't needed to make a single swap, we
-            # can just exit the main loop.
-            return
+def merge_sort(arr: List[int]) -> List[int]:
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return arr
+
+def my_new_sorting_function(arr: List[int]) -> None:
+    arr = merge_sort(arr)
+    return
 
 class ValidationError(Exception):
     pass
